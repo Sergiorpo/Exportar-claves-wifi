82e633ef1880a131531ae815da28ef432e7f23be
Sergio Rodriguez <44123331+Sergiorpo@users.noreply.github.com>
2022-07-02 13:43:36 +0200

    Add files via upload

---
+++ extraer_clave_wifi.ps1
@@ -0,0 +1,16 @@
+﻿Write-Output "Lista de redes Wifi."
+netsh wlan show profile | findstr "todos"  
+$data = @()
+
+while($clave -ne 'salir')
+{   
+    $clave = Read-Host "Introduce SSID"
+    netsh wlan show profile name=$clave Key=clear | Select-String -Pattern "Nombre de SSID"
+    netsh wlan show profile name=$clave Key=clear | Select-String -Pattern "Contenido"
+    $data += netsh wlan show profile name=$clave Key=clear | Select-String -Pattern "Nombre de SSID" 
+    $data += netsh wlan show profile name=$clave Key=clear | Select-String -Pattern "Contenido" 
+    $data += " "
+}
+$data | Out-File -Append Clave_wifi.txt  
+Write-Output "Guardando fichero con claves..."
+$end = Read-Host "Pulsa cualquier tecla para salir..."
